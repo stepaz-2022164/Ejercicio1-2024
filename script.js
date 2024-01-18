5a9967debea3f54b1ecffd21edea52ac47dfac53
stepaz-2022164 <stepaz-2022164@kinal.edu.gt>
2024-01-18 17:28:02 -0600

    commit 4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const palabras = ["KINAL", "JAVA", "MONITOR", "TECLADO", "MOUSE", "JAVASCRIPT", "COMPUTADORA" ]; 
+const palabras = ["KINAL", "JAVA", "MONITOR", "TECLADO", "MOUSE", "JAVASCRIPT", "COMPUTADORA", "PROCESADOR", "PROFESOR", "SEXTO" ]; 
 let palabraSeleccionada = palabras[Math.floor(Math.random() * palabras.length)]; 
 let letrasAdivinadas = [];
 let oportunidades = 7;
@@ -19,11 +19,36 @@ function dibujar() {
     }
 
     function actualizarDisplayPalabra() {
-        
+        let displayPalabra = "";
+        for (let letra of palabraSeleccionada) {
+          if (letrasAdivinadas.includes(letra)) {
+            displayPalabra += letra + " ";
+          } else {
+            displayPalabra += "_ ";
+          }
+        }
+        document.getElementById("displayPalabra").textContent = displayPalabra;
     }
 
-    function seleccionarLetra() {
-
+    function seleccionarLetra(letra) {
+        if (!letrasAdivinadas.includes(letra)) {
+            letrasAdivinadas.push(letra);
+            if (!palabraSeleccionada.includes(letra)) {
+              errores++;
+              drawHangman();
+            }
+            actualizarDisplayPalabra();
+            document.getElementById("letrasUsadas").textContent = letrasAdivinadas.join(", ");
+            document.getElementById("oportunidadesRestantes").textContent = 7 - errores;
+        
+            if (errores === 7) {
+              alert("Perdiste :( , La palabra era: " + palabraSeleccionada);
+              location.reload();
+            } else if (!document.getElementById("displayPalabra").textContent.includes("_")) {
+              alert("Ganaste :D");
+              location.reload();
+            }
+          }
     }
       
     for (let i = 65; i <= 90; i++) {
